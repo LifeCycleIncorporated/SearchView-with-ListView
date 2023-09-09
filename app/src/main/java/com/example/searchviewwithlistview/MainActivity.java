de4f41534d4de2f11840a046c69d18f5eabf75e4
Asadu <techasadbd@gmail.com>
2023-09-09 20:51:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/searchviewwithlistview/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.searchviewwithlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private SearchView searchView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewId);
+
+        searchView = findViewById(R.id.searchViewId);
+
+        String[] countryNames = getResources().getStringArray(R.array.countryName);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this,R.layout.simple_view,R.id.simple_textViewId,countryNames);
+        listView.setAdapter(arrayAdapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                arrayAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+    }
+}
